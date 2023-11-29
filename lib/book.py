146da94945ff1d3d8e92c3b793c32e5c3a276b28
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-29 22:19:06 +0300

    COMPLETED

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title="", page_count=0):
+        self.title = title
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand="", size=0):
+        self.brand = brand
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        self.condition = "New"
+        return "The shoe has been repaired."
+    
+    def cobble(self):
+        repair_msg = self.repair() 
+        return repair_msg
     pass
